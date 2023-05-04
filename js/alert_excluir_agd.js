0f2e0253fb550f718e3af1c936e7b1f33b7a6bd0
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2023-05-03 22:35:29 -0300

    trabalhando no alert pg paciente

---
+++ js/alert_excluir_agd.js
@@ -0,0 +1,6 @@
+//função que ao clicar no botao vermelho aparece a janela de confirmação
+function btnExcluir() {
+    const janelaExcluir = document.getElementById("janela_excluir");
+    janelaExcluir.style.display = "block";
+}
+
