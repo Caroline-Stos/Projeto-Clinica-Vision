67bbfc02f9fb20a4ec6c164f1465eb45abfaaf06
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2023-05-04 21:46:01 -0300

    trabalhando no js da pg paciente

--- js/alert_excluir_agd.js
+++ js/alert_excluir_agd.js
@@ -1,6 +1,24 @@
-//função que ao clicar no botao vermelho aparece a janela de confirmação
-function btnExcluir() {
-    const janelaExcluir = document.getElementById("janela_excluir");
-    janelaExcluir.style.display = "block";
-}
 
+const janelaCancelar = document.getElementById("janela_cancelar");
+
+//função que ao clicar no botao vermelho aparece o alert de confirmação
+function btnCancelar() {
+    janelaCancelar.style.display = "block";
+}
+//função que ao clicar no botao vermelho o alert fecha
+function btnFechar() {
+    janelaCancelar.style.display = "none";
+}
+// função que ao clicar fora da janela fecha o alert
+window.onclick = function(event) {
+    if (event.target == janelaCancelar) {
+       janelaCancelar.style.display = "none";
+    }
+    }
+
+
+const btnConfirmar = document.getElementById("btn_confirmar");
+
+function confirmarCancelamento() {
+    
+}
